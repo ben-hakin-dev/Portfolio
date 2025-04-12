d9c2793b0ec720889948a67d153486e8871e0e64
Ben Hakin <bh.01@Bens-MacBook-Air.local>
2025-04-12 16:46:22 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './reset.css';
+import './fonts.css';
+import './settings.css';
+import './layout.css';
+import './components.css';
+import './style.css';
+
+document.getElementById('clickMe').addEventListener('click', () => {
+  alert('Button clicked');
+});
